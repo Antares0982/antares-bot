5cfa013a1b63aa30bb8d1bfead4990853a6491a7
antares <antares@alyr.dev>
2024-05-05 22:27:43 +0800

    add sample cfg

---
+++ sample_bot_cfg.py
@@ -0,0 +1,25 @@
+
+class BasicConfig(object):
+    """
+    Basic sample config.
+    TOKEN and MASTER_ID must be provided.
+    Details refers to bot_default_cfg.py.
+    """
+    # LOCALE = 'en'
+    # DATA_DIR = "data"
+    TOKEN = "abcdef:123456"
+    MASTER_ID = "123456789"
+
+
+class AntaresBotConfig(object):
+    """
+    Config to control the bot behavior.
+    Details refers to bot_default_cfg.py.
+    """
+    # SKIP_PIKA_SETUP = True
+    # SKIP_LOAD_ALL_INTERNAL_MODULES = True
+    # SKIP_LOAD_ALL_MODULES = True
+    # SKIP_LOAD_INTERNAL_MODULE_{module_name_upper_case} = True
+    # SKIP_LOAD_MODULE_{module_name_upper_case} = True
+    # OBJGRAPH_TRACE_AT_START = True
+    pass
