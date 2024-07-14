5834f84bc73e0f4a6ac15cb441ac0f09e7356ed8
antares <antares@alyr.dev>
2024-07-14 18:59:48 +0800

    Update nix dep version hash

--- py_requirements.nix
+++ py_requirements.nix
@@ -1,16 +1,18 @@
 pkgs: pypkgs: with pypkgs;
 [
-  (python-telegram-bot.overrideAttrs {
+  (python-telegram-bot.overrideAttrs (super: {
     src = pkgs.fetchFromGitHub {
+      # antares-ptb
       owner = "Antares0982";
       repo = "python-telegram-bot";
       rev = "v21.3";
-      sha256 = "sha256-GN27mhR1V0jpI8MSsPLBUbdVQbQOKh4l29kwZ7yrACY=";
+      sha256 = "sha256-oCkwqN259q40L4FAReFySgbMjPi/GWuGmZP6PC+6VFw=";
     };
     doCheck = false;
-    doInstallCheck = false; 
-  })
+    doInstallCheck = false;
+    propagatedBuildInputs = super.propagatedBuildInputs ++ (with pypkgs;[ hatchling ]);
+  }))
   aiosqlite
   objgraph
-  aio-pika  # optional
+  aio-pika # optional
 ]
