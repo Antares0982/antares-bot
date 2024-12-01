9bf42966dd8ae18f2700d8a8dbf23c4371d1adbc
antares <antares@alyr.dev>
2024-12-01 23:22:01 +0800

    Add flake

---
+++ default.nix
@@ -0,0 +1,46 @@
+{
+  setuptools,
+  pythonOlder,
+  python-telegram-bot,
+  aiosqlite,
+  objgraph,
+  apscheduler,
+  pytz,
+  aio-pika,
+  pkgs,
+  buildPythonPackage,
+  hatchling,
+  setuptools-scm,
+  ...
+}:
+buildPythonPackage {
+  pname = "antares-bot";
+  version = "0.1.0";
+  src = ./.;
+  pyproject = true;
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+  disabled = pythonOlder "3.10";
+
+  dependencies = [
+    (python-telegram-bot.overrideAttrs (super: {
+      src = pkgs.fetchFromGitHub {
+        # antares-ptb
+        owner = "Antares0982";
+        repo = "python-telegram-bot";
+        rev = "v21.5";
+        sha256 = "sha256-xQLaiMh+O8NGHSgGvQVPzsXxfbN60EmIhYTRaDd2zPg=";
+      };
+      doCheck = false;
+      doInstallCheck = false;
+      propagatedBuildInputs = super.propagatedBuildInputs ++ (with pkgs; [ hatchling ]);
+    }))
+    aiosqlite
+    objgraph
+    apscheduler
+    pytz
+    aio-pika
+  ];
+}
