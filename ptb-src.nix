9c2353a5ca93dc4fa6ec456211fbba4f071a786b
antares <antares@alyr.dev>
2025-09-14 01:55:40 +0800

    Bump ptb to v22.4

--- ptb-src.nix
+++ ptb-src.nix
@@ -2,6 +2,6 @@
   # antares-ptb
   owner = "Antares0982";
   repo = "python-telegram-bot";
-  rev = "v22.3";
-  sha256 = "sha256-zVAcl+3KfLgXQ7FcGVHgg2FZ4P7uewDKL47Jzy3gXKI=";
+  rev = "v22.4";
+  sha256 = "sha256-5e8kJfBpkzepq8586FWF4qBVEyFcDc9KtlETCwyQu4A=";
 }
