5d5a133cd9ada7f3d20d94f2be80b53eeb2ab6f1
antares <antares@alyr.dev>
2026-08-22 22:00:50 +0800

    update ptb

--- ptb-src.nix
+++ ptb-src.nix
@@ -2,6 +2,6 @@
   # antares-ptb
   owner = "Antares0982";
   repo = "python-telegram-bot";
-  rev = "v22.7";
-  sha256 = "sha256-D3topfYuLvdBB7lPc+Qq046JAtmE6vZNOaS3rM1ZFyE=";
+  rev = "v22.8";
+  sha256 = "sha256-LrYlzjuL5j75G0TJXMjrL6bMAS+VkdAlfF1l3ODjqeU=";
 }
